9b242edfdc29a315c750b31a7290e8f1140bfd4f
RileyRedpath <rlyrdpth@gmail.com>
2017-02-12 20:48:01 -0600

    Add files via upload

---
+++ server/SaltnHash_SFS2X/MainExtension.java
@@ -0,0 +1,39 @@
+package riley.SaltnHash_SFS2X;
+
+import com.smartfoxserver.v2.extensions.SFSExtension;
+import com.smartfoxserver.v2.components.login.*;
+import com.smartfoxserver.v2.db.SFSDBManager;
+
+import java.util.Arrays;
+
+
+public class MainExtension extends SFSExtension {
+	
+	private LoginAssistantComponent lac;
+	public SFSDBManager DB;
+	
+	@Override
+	public void init() {
+		
+		lac = new LoginAssistantComponent(this);
+		
+		this.addRequestHandler("MessageSent", MessageSentHandler.class);
+		this.addRequestHandler("MessageRequest", MessageRequestHandler.class);
+        
+        lac.getConfig().loginTable = "users";
+        lac.getConfig().userNameField = "username";
+        lac.getConfig().passwordField = "password";//"hash";
+        lac.getConfig().extraFields = Arrays.asList("salt", "message");
+        lac.getConfig().customPasswordCheck = true;
+        lac.getConfig().preProcessPlugin = new LoginPreProcess();
+        //lac.getConfig().postProcessPlugin = new XLoginPostProcess();
+        DB = (SFSDBManager) getParentZone().getDBManager();
+        
+	}
+	
+	public void destroy(){
+		lac.destroy();
+		super.destroy();
+	}
+
+}
